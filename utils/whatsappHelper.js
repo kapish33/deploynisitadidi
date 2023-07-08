c31d8549880850356be5b875e8e3f78099d88cbb
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2023-07-08 12:52:14 +0530

    updated about us

--- utils/whatsappHelper.js
+++ utils/whatsappHelper.js
@@ -1,6 +1,6 @@
-export const number = '8707559369';
+export const number = '472802851';
 const message = `Hi, can you send me *program details*?`;
 const encodedMessage = encodeURIComponent(message);
-export const whatsAppURL = () => {
-  return `https://wa.me/+91${number}?text=${message}`;
+export const whatsAppURL = (enc = encodedMessage) => {
+  return `https://wa.me/+32${number}?text=${enc}`;
 };
