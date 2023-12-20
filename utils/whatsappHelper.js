ebef0307416021685362a4cacc786138d3109c00
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2023-12-20 21:48:59 +0530

    dome

--- utils/whatsappHelper.js
+++ utils/whatsappHelper.js
@@ -1,5 +1,5 @@
 export const number = '472802851';
-const message = `I would like to know more about the *Thought Leadership*`;
+const message = `I want to know more about programs during Gurudev's europe visit`;
 const encodedMessage = encodeURIComponent(message);
 export const whatsAppURL = (enc = encodedMessage) => {
   return `https://wa.me/+32${number}?text=${enc}`;
